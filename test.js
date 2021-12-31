4f7752970ee0965ada5720449adc56e2746f57de
Blitz.js CLI <noop@blitzjs.com>
2021-12-31 18:11:22 +0100

    Brand new Blitz app!

---
+++ test.js
@@ -0,0 +1,25 @@
+
+
+
+const randomInteger = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+(function () {
+
+  // create an array and push 10 ids from 1 to 10
+  var ids = [];
+  for (var i = 1; i <= 10; i++) {
+
+if(randomInteger(1,10) > randomInteger(1,10)){
+  ids.push(i);
+}
+
+
+
+
+
+
+
+})()
